a9bd5ca87ec8e31a6b52609eedeb77127326c520
Ko Takagi <knockoutmarch@gmail.com>
2018-08-31 12:27:19 +0900

    :ant: #3 Fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/mw-wp-form-line-notify.php';
+	require dirname( dirname( __FILE__ ) ) . '/snow-monkey-design-skin-gosky.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
